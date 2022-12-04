cdce747261559c1c42663119915521cfb87eb43b
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2022-12-04 12:51:30 +0100

    add random img on homepage

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="text-cyan-600 text-sm">
-      <div><a href='https://github.com/aimach'>
+      <div className="py-5"><a href='https://github.com/aimach' target='_blank' rel="noreferrer">
         Portefolio Marie-Hermine Schneider - 2022
       </a></div>
     </div>);
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 
@@ -10,6 +11,7 @@ export default function Home({ posts }) {
         <title>Mon sujet de porte-folio 1</title>
       </Head>
       <div className='flex flex-col items-center my-8'>
+        <img src='https://picsum.photos/600/200' alt='random img' />
         <h1 className='text-3xl font-bold my-8'>Homepage</h1>
         <p className='m-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <Link href='/articles'>
